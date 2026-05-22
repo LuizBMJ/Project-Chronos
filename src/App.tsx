60f4ad13a420cfb7b634b94cb05f71ba644ad313
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-05-22 14:57:52 -0400

    Adding Lucide-react, and altering the heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
 	return (
 		<>
-			<Heading />
+			<Heading>
+				Olá mundo!
+				<button>
+					<TimerIcon />
+				</button>
+			</Heading>
 			<p>
 				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas porro
 				modi, nulla dolore blanditiis, deleniti atque laudantium asperiores
@@ -14,4 +20,3 @@ export function App() {
 		</>
 	);
 }
-
