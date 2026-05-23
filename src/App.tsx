2fe63e4991b39f3f3d6533cc901a14d159dcfe48
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-05-22 22:23:41 -0400

    Cycles component created (The balls to show what time is going to be next)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './styles/theme.css';
 import './styles/global.css';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
 	return (
@@ -25,20 +26,24 @@ export function App() {
 			<Container>
 				<form className='form' action=''>
 					<div className='formRow'>
-						<DefaultInput type='text' id='task' labelText='Task' placeholder='Digite algo...' />
+						<DefaultInput
+							type='text'
+							id='task'
+							labelText='Task'
+							placeholder='Digite algo...'
+						/>
 					</div>
 
 					<div className='formRow'>
-					<p> Lorem ipsum dolor sit amet. </p>
+						<p> Lorem ipsum dolor sit amet. </p>
 					</div>
 
 					<div className='formRow'>
-					<p> Ciclos </p>
-					<p> 0 0 0 0 0 0 </p>
+						<Cycles />
 					</div>
 
 					<div className='formRow'>
-					<button> Enviar</button>
+						<button> Enviar</button>
 					</div>
 				</form>
 			</Container>
