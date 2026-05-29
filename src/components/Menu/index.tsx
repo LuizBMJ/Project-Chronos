fe12b22c673902d85918bb53c4051a708df61b6e
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-05-29 15:45:40 -0400

    Refactor: Better code organization and structure

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,46 @@
-import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon} from 'lucide-react';
+import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon, MoonIcon} from 'lucide-react';
 import styles from './styles.module.css';
+import { useState, useEffect} from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 function Menu() {
+    const [theme, setTheme] = useState<AvailableThemes>(() => {
+        const savedTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
+        return savedTheme;
+    });
+
+    const nextThemeIcon = {
+        dark: <SunIcon />,
+        light: <MoonIcon />
+    }
+
+    function handleToggleTheme(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        event.preventDefault();
+        setTheme((prevTheme) => {
+            const newTheme = prevTheme === 'dark' ? 'light' : 'dark';
+            return newTheme;
+        });
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+
     return <div className={styles.menu}>
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label="Ir para Home" title="Home" >
             <HouseIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label="Ir para Histórico" title="Histórico" >
             <HistoryIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
+        <a href="#" className={styles.menuLink} aria-label="Ir para Configurações" title="Configurações" >
             <SettingsIcon />
         </a>
-        <a href="#" className={styles.menuLink}>
-            <SunIcon />
+        <a href="#" className={styles.menuLink} id="theme-toggle" aria-label="Mudar Tema" title="Tema" onClick={handleToggleTheme} >
+            {nextThemeIcon[theme]}
         </a>
     </div>;
 }
