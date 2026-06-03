dba0991173209d79bce0dc1f91b36d2a41908163
Jorge Luiz <luiz.bevilacquam@gmail.com>
2026-06-03 17:19:58 -0400

    Feat: Created utils getNextCycle and applied it on mainForm

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,12 +2,65 @@ import { PlayCircleIcon } from 'lucide-react'
 import { Cycles } from '../Cycles'
 import { DefaultButton } from '../DefaultButton'
 import { DefaultInput } from '../DefaultInput'
+import { useRef } from 'react'
+import { getNextCycle } from '../../utils/getNextCycle'
+import type { TaskModel } from '../../models/TaskModel'
+import { useTaskContext } from '../../contexts/TaskContext/UseTaskContext'
 
 export function MainForm() {
+  const { state, setState } = useTaskContext()
+  const taskNameInput = useRef<HTMLInputElement>(null)
+
+  // ciclos
+  const nextCycle = getNextCycle(state.currentCycle)
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if (taskNameInput.current === null) return
+
+    const taskName = taskNameInput.current.value.trim()
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa')
+      return
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interuptedDate: null,
+      duration: 1,
+      type: 'workTime',
+    }
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining, // Conferir
+        formattedSecondsRemaining: '00:00', // Conferir
+        tasks: [...prevState.tasks, newTask],
+      }
+    })
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
-        <DefaultInput labelText='task' id='meuInput' type='text' placeholder='Digite algo' />
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
+          ref={taskNameInput}
+        />
       </div>
 
       <div className='formRow'>
